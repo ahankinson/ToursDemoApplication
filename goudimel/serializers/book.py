a5c74ca8ede3ba45c2996aa66b9c6309864332c1
Andrew Hankinson <andrew.hankinson@gmail.com>
2013-11-09 11:07:30 -0500

    Updated serializers

--- goudimel/serializers/book.py
+++ goudimel/serializers/book.py
@@ -2,6 +2,5 @@ from goudimel.models.book import Book
 from rest_framework import serializers
 
 class BookSerializer(serializers.HyperlinkedModelSerializer):
-
     class Meta:
         model = Book
--- goudimel/serializers/piece.py
+++ goudimel/serializers/piece.py
@@ -1,7 +1,11 @@
 from goudimel.models.piece import Piece
+from goudimel.serializers.book import BookSerializer
+
 from rest_framework import serializers
 
 class PieceSerializer(serializers.HyperlinkedModelSerializer):
+    book_id = BookSerializer()
+    composer_name = serializers.Field(source="composer_name")
 
     class Meta:
         model = Piece
